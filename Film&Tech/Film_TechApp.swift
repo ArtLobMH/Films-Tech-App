989d362f09b7f264a01973ca92da49e67f3347bc
Artem Lobanov <lobanovnorm@gmail.com>
2023-06-05 15:49:34 +0300

    Initial Commit

---
+++ Film&Tech/Film_TechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film_TechApp.swift
+//  Film&Tech
+//
+//  Created by Макбук on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Film_TechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
