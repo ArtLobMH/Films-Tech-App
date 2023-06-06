fa825883390c048161db07e07577acc8f09cde6a
Artem Lobanov <lobanovnorm@gmail.com>
2023-06-06 09:58:14 +0300

    Первый коммит: рабочий TableView, болванка ScrollView для FilmsView

--- Film&Tech/ContentView.swift
+++ Film&Tech/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            FilmsView()
+                .tabItem{
+                    Image("list")
+                    Text("Top Films")
+                }
+            CategoryView()
+                .tabItem{
+                    Image("cart")
+                    Text("Category")
+                }
+            CompareView()
+                .tabItem{
+                    Image("compare")
+                    Text("Compare")
+                }
         }
-        .padding()
+    }
+}
+
+struct FilmsView: View {
+    var body: some View {
+        ZStack {
+            Color.gray
+            ScrollView{
+                VStack {
+                    ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                        FilmCard()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct FilmCard: View {
+    var body: some View {
+        Rectangle()
+            .frame(height : 250)
+            .background(Color.black)
+        .padding(.bottom, 20)
+    }
+}
+
+struct CategoryView: View {
+    var body: some View {
+        Text("Category")
+    }
+}
+
+struct CompareView: View {
+    var body: some View {
+        Text("Compare")
     }
 }
 
